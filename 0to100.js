796ccf16ce00dd0214505fd70849f8f2e7337730
Keenanvb <keenanvanblerk@gmail.com>
2020-11-03 15:47:57 +0200

    Update 0to100

--- 0to100.js
+++ 0to100.js
@@ -2,15 +2,14 @@
     Returns an array 0-100
 */
 
-let count = () => {
-   let result = [];
+const count = () => {
+   const result = [];
    for (let i = 100; i >= 0; i--) {
       result.push(100 - i);
    }
-   //console.log(result);
    return result;
 };
 
-let res = count();
+const res = count();
 
 console.log('res', res);
