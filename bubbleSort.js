0b1af144664118da4ce4577103209f9df0f6f6dc
Keenanvb <keenanvanblerk@gmail.com>
2020-11-03 16:09:52 +0200

    update bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,24 +1,23 @@
-let arr = [5,8,9,55,44,22,115,5,1,6,8,4];
+const arr = [5, 8, 9, 55, 44, 22, 115, 5, 1, 6, 8, 4];
 
-let bubbleSort = (arr) => {
-    let swopped;
+const bubbleSort = (arr) => {
+   let swopped;
 
-    do {
-        swopped = false;
-        for (let i = 0; i <= arr.length; i++) {
-            if (arr[i] > arr[i + 1]) {
-                let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                swopped = true;
-            }
+   do {
+      swopped = false;
+      for (let i = 0; i <= arr.length; i++) {
+         if (arr[i] > arr[i + 1]) {
+            let temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+            swopped = true;
+         }
+      }
+   } while (swopped);
 
-        }
-    } while (swopped)
+   return arr;
+};
 
-    return arr;
-}
-
-let sortedArr = bubbleSort(arr);
+const sortedArr = bubbleSort(arr);
 
 console.log('sortedArr ', sortedArr);
