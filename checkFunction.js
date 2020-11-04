c3a9ac88c01a33f8b8410cfcf6b91d8ae4deb477
Keenanvb <keenanvanblerk@gmail.com>
2020-11-04 13:53:38 +0200

    update checkFunction

--- checkFunction.js
+++ checkFunction.js
@@ -3,30 +3,30 @@ Check if a function is valid
 */
 const str = '{{}{}{}{}}';
 
-let checkBracks = (brackets) => {
-    let num = 0;
-    let flag = false;
-    if (brackets.startsWith('{') && brackets.endsWith('}')) {
-        for (let i of brackets) {
-            if (i === '{') {
-                num += 1;
-            } else if (i === '}') {
-                num -= 1;
-            }
-        }
-    } else {
-        return flag;
-    }
+const checkBracks = (brackets) => {
+   let num = 0;
+   const flag = false;
+   if (brackets.startsWith('{') && brackets.endsWith('}')) {
+      for (let i of brackets) {
+         if (i === '{') {
+            num += 1;
+         } else if (i === '}') {
+            num -= 1;
+         }
+      }
+   } else {
+      return flag;
+   }
 
-    return isFuctionValid(num);
-}
+   return isFuctionValid(num);
+};
 
-let isFuctionValid = (num) => {
-    let check = false;
-    check = (num === 0 ? true : false);
-    return check;
-}
+const isFuctionValid = (num) => {
+   let check = false;
+   check = num === 0 ? true : false;
+   return check;
+};
 
-let flag = checkBracks(str);
+const flag = checkBracks(str);
 
-console.log(`Function valid: ${flag}`)
+console.log(`Function valid: ${flag}`);
