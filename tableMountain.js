f11a82b4d0ba22bbb3e76165a4ded4526e49f598
Keenanvb <keenanvanblerk@gmail.com>
2021-05-13 15:50:19 +0200

    add table mountain

---
+++ tableMountain.js
@@ -0,0 +1,19 @@
+var landscape = function () {
+   var result = '';
+   var flat = function (size) {
+      for (var count = 0; count < size; count++) result += '_';
+   };
+   var mountain = function (size) {
+      result += '/';
+      for (var count = 0; count < size; count++) result += " '";
+      result += '\\';
+   };
+   flat(3);
+   mountain(5);
+   flat(6);
+   mountain(1);
+   flat(1);
+   return result;
+};
+
+console.log('', landscape());
